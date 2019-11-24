438ff3daa8801b01b361be8ac8105a6151af47cb
lig001 <lig001@cmrh.com>
2019-11-24 13:55:54 +0800

    change service discovery module

--- service/src/main/java/service/ServiceDiscoveryApplication.java
+++ service/src/main/java/service/ServiceDiscoveryApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class ServiceApplication {
+public class ServiceDiscoveryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServiceApplication.class, args);
+        SpringApplication.run(ServiceDiscoveryApplication.class, args);
     }
 
 }
