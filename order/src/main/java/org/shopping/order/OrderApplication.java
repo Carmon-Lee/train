f4839a9eb8898d51eefaca5fe2874268edf130df
lig001 <lig001@cmrh.com>
2019-11-24 16:48:17 +0800

    添加到order服务到服务发现

--- order/src/main/java/org/shopping/order/OrderApplication.java
+++ order/src/main/java/org/shopping/order/OrderApplication.java
@@ -3,8 +3,10 @@ package org.shopping.order;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("org.shopping.order.dao")
 public class OrderApplication {
 
--- order/src/test/java/org/shopping/order/service/OrderServiceTest.java
+++ order/src/test/java/org/shopping/order/service/OrderServiceTest.java
@@ -1,14 +1,12 @@
 package org.shopping.order.service;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.shopping.order.entity.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 class OrderServiceTest {
@@ -18,7 +16,6 @@ class OrderServiceTest {
 
     @Test
     void getByPk() {
-
         List<Order> byPk = orderService.getByPk();
     }
 }
